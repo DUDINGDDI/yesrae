a773b98a465ddb7059bf29a32147660f2c6a9a5c
김도현 <beigemint26@gmail.com>
2023-10-04 10:31:51 +0900

    Feat: redis 비밀번호 설정, CORS 수정

--- fast-api/project/src/config/redis_config.py
+++ fast-api/project/src/config/redis_config.py
@@ -12,7 +12,7 @@ def redis_config():
         REDIS_PORT = os.getenv("REDIS_PORT")
         REDIS_DATABASE = os.getenv("REDIS_DATABASE")
         REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
-        rd = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DATABASE)
+        rd = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DATABASE, password=REDIS_PASSWORD)
 
         return rd
 
